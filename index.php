86e0c04b60a68380841c27eeee35bb1a152e9a70
Xiang Gao <phoenix.x.gao@gmail.com>
2013-11-19 17:45:54 +0800

    update

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 require 'vendor/autoload.php';
+require 'app/config.php';
 
 $app = new \Slim\Slim();
+
+require 'app/routes/login.php';
+require 'app/routes/homepage.php';
 
 $app->get('/hello/:name(/)', function ($name) {
     echo "Hello, $name";
